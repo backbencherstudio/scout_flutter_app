60c85b0da6bbceb0bbab9670522f3d025e60bb28
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-02-12 10:58:06 +0600

    Tab bar added

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class MyApp extends StatelessWidget {
                   titleTextStyle: GoogleFonts.poppins(
                     fontSize: 28.sp,
                     fontWeight: FontWeight.w600,
-                    color: Colors.white,
+                    color: Colors.black,
                   )
                 ),
                 //  setting-up textTheme globally according to figma and use screen_util feature for responsiveness
@@ -121,12 +121,12 @@ class MyApp extends StatelessWidget {
               debugShowCheckedModeBanner: false,
               initialRoute: RouteName.homeScreen,
               routes: {
-                '/': (context) => SplashScreen(),
+               // '/': (context) => SplashScreen(),
                 RouteName.homeScreen : (context) => HomeScreen(),
-                RouteName.profileScreen : (context) => ProfileScreen(),
-                RouteName.profileSettingScreen : (context) => ProfileSettingScreen(),
-                RouteName.loginScreen : (context) => LoginScreen(),
-                RouteName.signUpScreen : (context) => SignUpScreen(),
+               // RouteName.profileScreen : (context) => ProfileScreen(),
+               // RouteName.profileSettingScreen : (context) => ProfileSettingScreen(),
+               // RouteName.loginScreen : (context) => LoginScreen(),
+              //  RouteName.signUpScreen : (context) => SignUpScreen(),
               }
             // home: HomeScreen(),
           );
