622cca945a18b7b5820b3581b2e53efe34287f1a
salmanbashar1009 <salmanbashar101@gmail.com>
2025-02-12 13:43:45 +0600

    text field added in my details screen

--- lib/view/profile_setting_ui/my_details_screen.dart
+++ lib/view/profile_setting_ui/my_details_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:scout_app/utils/custom_text_field.dart';
 import 'package:scout_app/view/profile_setting_ui/widgets/custom_app_bar.dart';
+import 'package:scout_app/view/profile_setting_ui/widgets/styles.dart';
 
 class MyDetailsScreen extends StatelessWidget {
   const MyDetailsScreen({super.key});
@@ -16,8 +18,22 @@ class MyDetailsScreen extends StatelessWidget {
               CustomAppBar(title: "My Details"),
               SizedBox(height: 32.h,),
               TextFormField(
-
-              )
+                decoration: textFieldDecoration(context, "Full Name", null)
+              ),
+              SizedBox(height: 24.h,),
+              TextFormField(
+                  decoration: textFieldDecoration(context, "Username", null)
+              ),
+              SizedBox(height: 24.h,),
+              TextFormField(
+                  decoration: textFieldDecoration(context, "Email Address", null)
+              ),
+              SizedBox(height: 24.h,),
+              TextFormField(
+                maxLines: 3,
+                  decoration: textFieldDecoration(context, "Bio", null)
+              ),
+              Spacer(),
             ],
           ),
         ),
