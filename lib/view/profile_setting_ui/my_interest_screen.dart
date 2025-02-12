6dc13f650fe709706c3e1e293a079bc2c248e8d9
salmanbashar1009 <salmanbashar101@gmail.com>
2025-02-12 15:04:31 +0600

    my interest screen added

--- lib/view/profile_setting_ui/my_interest_screen.dart
+++ lib/view/profile_setting_ui/my_interest_screen.dart
@@ -4,8 +4,8 @@ import 'package:scout_app/utils/custom_text_field.dart';
 import 'package:scout_app/utils/custom_app_bar.dart';
 import 'package:scout_app/view/profile_setting_ui/widgets/styles.dart';
 
-class MyDetailsScreen extends StatelessWidget {
-  const MyDetailsScreen({super.key});
+class MyInterestScreen extends StatelessWidget {
+  const MyInterestScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,21 +18,10 @@ class MyDetailsScreen extends StatelessWidget {
               CustomAppBar(title: "My Details"),
               SizedBox(height: 32.h,),
               TextFormField(
-                decoration: textFieldDecoration(context, "Full Name", null)
+                decoration: textFieldDecoration(context, "Search", ImageIcon(AssetImage("assets/icons/search.png"),size: 24,))
               ),
-              SizedBox(height: 24.h,),
-              TextFormField(
-                  decoration: textFieldDecoration(context, "Username", null)
-              ),
-              SizedBox(height: 24.h,),
-              TextFormField(
-                  decoration: textFieldDecoration(context, "Email Address", null)
-              ),
-              SizedBox(height: 24.h,),
-              TextFormField(
-                maxLines: 3,
-                  decoration: textFieldDecoration(context, "Bio", null)
-              ),
+              SizedBox(height: 20.h,),
+
               Spacer(),
               SizedBox(height: 20.h,),
               TextButton(onPressed: (){}, child: Text("Cancel",style: Theme.of(context).textTheme.bodyLarge,)),
--- lib/view/profile_setting_ui/widgets/styles.dart
+++ lib/view/profile_setting_ui/widgets/styles.dart
@@ -4,8 +4,10 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 InputDecoration textFieldDecoration(BuildContext context ,String label, Widget? suffixIcon ){
   return InputDecoration(
+
+    alignLabelWithHint: true,
     labelText: label,
-    labelStyle: Theme.of(context).textTheme.bodySmall!.copyWith(color: Color(0xFF4B5155)),
+    labelStyle: Theme.of(context).textTheme.bodySmall!.copyWith(color: Color(0xFF4B5155),),
     floatingLabelStyle: Theme.of(context).textTheme.bodySmall!.copyWith(color: Color(0xFF000000),fontSize: 16.sp),
     enabledBorder: OutlineInputBorder(
       borderRadius: BorderRadius.circular(4.r),
@@ -19,6 +21,10 @@ InputDecoration textFieldDecoration(BuildContext context ,String label, Widget? 
         borderRadius: BorderRadius.circular(4.r),
         borderSide: BorderSide(color: Color(0xFF9BA2A7))
     ),
-    suffixIcon: suffixIcon,
+    suffixIcon: Padding(
+      padding:  EdgeInsets.only(right: 8.0),
+      child: suffixIcon,
+    ),
+    suffixIconConstraints: BoxConstraints(maxWidth: 32,maxHeight: 32),
   );
 }
