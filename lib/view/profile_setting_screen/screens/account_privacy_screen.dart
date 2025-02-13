b7d74333fe6e3335a0d9d33e95ab830687f15187
salmanbashar1009 <salmanbashar101@gmail.com>
2025-02-13 10:39:18 +0600

    account privacy UI completed

--- lib/view/profile_setting_screen/screens/account_privacy_screen.dart
+++ lib/view/profile_setting_screen/screens/account_privacy_screen.dart
@@ -31,22 +31,35 @@ class _AccountPrivacyScreenState extends State<AccountPrivacyScreen> {
                     fontWeight: FontWeight.w400
                   ),),
                   Spacer(),
-                  Switch(
-                    activeColor: Colors.green,
-                    inactiveTrackColor: Color(0xFFC6CACD) ,
-                    activeTrackColor: Color(0xFFFB6012) ,
-                    trackOutlineColor: WidgetStateColor.transparent,
-                    thumbColor: const WidgetStatePropertyAll<Color>(Colors.white),
-                    value: light,
-                    onChanged: (bool value) {
-                      setState(() {
-                        light = value;
-                      });
-                    },
+                  Transform.scale(
+                    scale: 0.75,
+                    child: Switch(
+                      thumbIcon: WidgetStateProperty.all(
+                        Icon(Icons.circle, size: 20,color: Colors.white,), // Fixed icon with fixed size
+                      ),
+                      activeColor: Colors.green,
+                      inactiveTrackColor: Color(0xFFC6CACD) ,
+                      activeTrackColor: Color(0xFFFB6012) ,
+                      trackOutlineColor: WidgetStateColor.transparent,
+                      thumbColor: const WidgetStatePropertyAll<Color>(Colors.white),
+                      value: light,
+                      onChanged: (bool value) {
+                        setState(() {
+                          light = value;
+                        });
+                      },
+                    ),
                   ),
+                  SizedBox(height: 24.h,),
 
                 ],
+              ),
+              Text("When your account is public, others will be able to see your profile, events you have created and ones you have marked as “interested”.",
+              style: Theme.of(context).textTheme.bodySmall,
+              maxLines: 5,
+                softWrap: true,
               )
+
             ]
           ),
         )
